76a68091c2fed09037eb63e4e5beb163d883e2fa
xx <xmr93213@qq.com>
2017-01-11 08:42:40 +0800

    Initial commit

---
+++ cache/admin/footer.htm.php
@@ -0,0 +1,13 @@
+<?php /* Smarty version 2.6.26, created on 2017-01-10 09:16:45
+         compiled from footer.htm */ ?>
+<div class="clear"></div>
+<div id="dcFooter">
+ <div id="footer">
+  <div class="line"></div>
+  <ul>
+   <?php echo $this->_tpl_vars['lang']['footer_copyright']; ?>
+
+  </ul>
+ </div>
+</div><!-- dcFooter 结束 -->
+<div class="clear"></div>
---
+++ cache/m/ur_here.tpl.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.26, created on 2017-01-10 09:32:22
+         compiled from inc/ur_here.tpl */ ?>
+<div class="urHere"><?php echo $this->_tpl_vars['lang']['ur_here']; ?>
+：<?php echo $this->_tpl_vars['ur_here']; ?>
+</div>
